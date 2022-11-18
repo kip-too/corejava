495d2e995096ea3c1df19c80871f80098c535367
kurgatkiptoo <hoseakiptoo201@gmail.com>
2022-11-18 16:23:56 +0300

    GameEntry array structure

---
+++ DataStructs/Arrays/GameEntry.java
@@ -0,0 +1,23 @@
+package DataStructs.Arrays;
+
+public class GameEntry {
+    protected String name;
+    protected int score;
+
+    public GameEntry(String n, int s)
+    {
+     this.name=n;
+     this.score=s;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getScore(){
+        return score;
+    }
+
+    public String toString(){
+        return "("+name+","+score+")";
+    }
+}
