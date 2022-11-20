d3271cd0d717e1d2f7608162c63c0e180869b49b
kurgatkiptoo <hoseakiptoo201@gmail.com>
2022-11-20 14:08:24 +0300

    generic pairs

--- generics/Pair.java
+++ generics/Pair.java
@@ -1,6 +1,6 @@
 package generics;
 
-public class Pair<R,S>{
+ class Pair<R,S>{
     public R fst;
     public S snd;
     public Pair(R fst, S snd){
@@ -11,4 +11,13 @@ public class Pair<R,S>{
     public String toString(){
         return "("+fst+","+snd+")";
     }
+
+public static void main(){
+Pair<Integer,String> pintstr = new Pair<Integer,String>(4, "yo");
+System.out.println("pintstr:"+pintstr);
+pintstr.fst=12;
+int left = pintstr.fst;
+String right= pintstr.snd;
+System.out.println("as parts : fst=" +left+",snd="+right);
 }
+ }
