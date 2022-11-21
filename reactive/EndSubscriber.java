de91d53ea6fadc9577501652c7e47030d0cc820d
kurgatkiptoo <hoseakiptoo201@gmail.com>
2022-11-21 18:50:22 +0300

    Flow API

---
+++ reactive/EndSubscriber.java
@@ -0,0 +1,47 @@
+package reactive;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.Flow.Subscriber;
+import java.util.concurrent.Flow.Subscription;
+
+public class EndSubscriber <T> implements Subscriber<T>{
+    private Subscription subscription;
+    public List<T> consumedElements = new LinkedList<>();
+
+        
+    
+    @Override
+    public void onSubscribe(Subscription subscription) {
+        this.subscription = subscription;
+        subscription.request(1);
+        
+    }
+
+
+
+    @Override
+    public void onComplete() {
+        System.out.println("Done");
+        
+    }
+
+
+
+    @Override
+    public void onError(Throwable t) {
+        
+        t.printStackTrace();
+    }
+
+
+
+    @Override
+    public void onNext(T item) {
+       System.out.println("Got:" +item);
+        
+    }
+
+    
+    
+}
